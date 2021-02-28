573ed1ed4a6a6d1390576934951ad21055070116
Easkandar Petra <56609126+easkandarpetra@users.noreply.github.com>
2021-02-28 22:31:08 +0800

    Edit wire.write()

--- modbus.ino
+++ modbus.ino
@@ -4,12 +4,12 @@
 
 SoftwareSerial mySerial(3,4); //Rx: Pin 3, Tx: Pin 4
 
-uint16_t m_startAddress[12] = {0x0000 , 0x0006, 0x000C, 0x001E, 0x0012, 0x0018, 0x0046, 0x0034, 0x003E, 0x0102, 0x00E0, 0x0156};
+uint16_t m_startAddress[10] = {0x0000 , 0x0006, 0x000C, 0x001E, 0x0012, 0x0018, 0x0046, 0x0156};
 uint16_t m_length = 0x00001;
 uint8_t result;
 uint8_t ret;
 int sta = 0;
-int DE = 7;
+int DE = 8;
 union
 {
   uint16_t u[2];
@@ -21,12 +21,15 @@ meterdata;
 
 char outBuffer[30];
 
-String volt = String(240);
-String current = String(2.5);
-String power = String(120);
-String powerFactor = String(0.9);
-String frequency = String(50);
-String output = volt + "," + current + "," + power + "," + powerFactor + "," + frequency;
+String volt = String(meterdata.f);
+String current = String(meterdata.f);
+String power = String(meterdata.f);
+String powerFactor = String(meterdata.f);
+String frequency = String(meterdata.f);
+String apparentPower = String(meterdata.f);
+String reactivePower = String(meterdata.f);
+String totalKwh = String(meterdata.f);
+
 
 ModbusMaster node;
 
@@ -45,9 +48,8 @@ void disableTx()  //RX
 void requestEvent() {
 
  // Put the data inside Wire.write()
-
-    Wire.write("240, 2.5, 120, 0.9, 50");
-    Serial.println(output);
+ 
+    Wire.write("230, 0.1, 12, 0.5, 22");
     
 }
 
@@ -58,8 +60,10 @@ void setup()
   node.preTransmission(enableTx);
   node.postTransmission(disableTx);
   Serial.begin(9600);
+  mySerial.begin(9600);
   node.begin(111, mySerial);
-  Serial.println("CS I2C slaves Testing");
+  Serial.println("CS I2C slaves Testing...");
+  delay(2000);
   
   // I2C Slave address
   Wire.begin( 0x5B );
@@ -75,120 +79,79 @@ void loop()
   Wire.requestFrom(requestEvent, 1);
   Wire.endTransmission();
 
-  while(Wire.available()){
+
     
   result = node.readInputRegisters(m_startAddress[sta], m_length);
-  Serial.println(result);
   if (result == node.ku8MBSuccess)
   {
       if (sta == 0)
         {
         meterdata.u[1] = node.getResponseBuffer(0);
         meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Voltage: ");
-        Serial.print(meterdata.f);
-        Serial.println("V");
-       // Serial.println(meterdata.u[1]);
+        volt = String(meterdata.f);
+        Serial.print(volt + ",");
         }
-      
       else if (sta == 1)
         {
         meterdata.u[1] = node.getResponseBuffer(0);
         meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Current: ");
-        Serial.print(meterdata.f);
-        Serial.println("A");
+        current = String(meterdata.f);
+        Serial.print(current + ",");
         }
       else if (sta == 2) 
        {
         meterdata.u[1] = node.getResponseBuffer(0);
         meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Power: ");
-        Serial.print(meterdata.f);
-        Serial.println("W");
+       power = String(meterdata.f);
+       Serial.print(power + ",");
        }
       else if (sta == 3) 
        {
         meterdata.u[1] = node.getResponseBuffer(0);
         meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Power Factor: ");
-        Serial.print(meterdata.f);
-        Serial.println("");
+        powerFactor = String(meterdata.f);
+        Serial.print(powerFactor + ",");
        }
        else if (sta == 4) 
        {
         meterdata.u[1] = node.getResponseBuffer(0);
         meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Apparent Power: ");
-        Serial.print(meterdata.f);
-        Serial.println("VA");
+        apparentPower = String(meterdata.f);
+        Serial.print(apparentPower + ",");
        }
        else if (sta == 5) 
        {
         meterdata.u[1] = node.getResponseBuffer(0);
         meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Reactive Power: ");
-        Serial.print(meterdata.f);
-        Serial.println("VAR");
+        reactivePower = String(meterdata.f);
+        Serial.print(reactivePower + ",");
        }
        else if (sta == 6) 
        {
         meterdata.u[1] = node.getResponseBuffer(0);
         meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Frequency of supply voltage: ");
-        Serial.print(meterdata.f);
-        Serial.println("Hz");
+        frequency = String(meterdata.f);
+        Serial.print(frequency + ",");
+        
        }
-       else if (sta == 7) 
+       else if (sta == 7)
        {
         meterdata.u[1] = node.getResponseBuffer(0);
         meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Total system power: ");
-        Serial.print(meterdata.f);
-        Serial.println("W");
-       }
-       else if (sta == 8)
-       {
-        meterdata.u[1] = node.getResponseBuffer(0);
-        meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Total system Power Factor: ");
-        Serial.print(meterdata.f);
+        totalKwh = String(meterdata.f);
+        Serial.print(totalKwh);
         Serial.println("");
-       }
-       else if (sta == 9)
-       {
-        meterdata.u[1] = node.getResponseBuffer(0);
-        meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Current Demand: ");
-        Serial.print(meterdata.f);
-        Serial.println("A");
-       }
-       else if (sta == 10)
-       {
-        meterdata.u[1] = node.getResponseBuffer(0);
-        meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Neutral Current: ");
-        Serial.print(meterdata.f);
-        Serial.println("A");
-       }
-       else if (sta == 11)
-       {
-        meterdata.u[1] = node.getResponseBuffer(0);
-        meterdata.u[0] = node.getResponseBuffer(1);
-        Serial.print("Total kWh: ");
-        Serial.print(meterdata.f);
-        Serial.println("kWh");
-       }
+       } 
   }
 
       else 
       {
       Serial.print("ERROR");
-     // Serial.println(result, HEX);
+     
       }
 
   sta++;
-  if (sta == 12) sta = 0;
+  if (sta == 8) sta = 0;
   delay(1000);
-}
+
 }
